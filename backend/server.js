998b8e6fb529514eddeb5493ac5fb51da2abe747
LENINDALLAS <lenindallas.ld@gmail.com>
2021-08-27 19:21:45 +0530

    updates

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routers/userRouter.js";
@@ -8,6 +9,7 @@ import orderRouter from "./routers/orderRouter.js";
 // import data from './data.js'
 dotenv.config();
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
